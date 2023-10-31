e45f6edecac63cabd73367f75307bc4a4e6bb5c5
AllenQCH <1181303285@qq.com>
2023-10-31 23:00:50 +0800

    首次上传

---
+++ aircraft-web/src/main/java/com/travel/web/Test1.java
@@ -0,0 +1,19 @@
+package com.travel.web;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+    }
+    void print(String str, int x){
+        String trim = str.trim();
+
+        String s = trim.replaceAll(" ", ",");
+        String[] split = s.split(",");
+        for(int i=0;i<split.length;i++){
+            
+        }
+
+
+    }
+}
